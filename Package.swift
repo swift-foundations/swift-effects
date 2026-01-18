b60bf3d4ae374b5eac91581d047f837a779ef336
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-01-18 20:17:01 +0100

    Migrate tests to nested package pattern

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v26),
         .tvOS(.v26),
         .watchOS(.v26),
-        .visionOS(.v26),
+        .visionOS(.v26)
     ],
     products: [
         .library(
@@ -23,55 +23,34 @@ let package = Package(
         .library(
             name: "Effects Testing",
             targets: ["Effects Testing"]
-        ),
+        )
     ],
     dependencies: [
         .package(path: "../../swift-primitives/swift-effect-primitives"),
         .package(path: "../../swift-primitives/swift-dependency-primitives"),
-        .package(path: "../../swift-primitives/swift-async-primitives"),
+        .package(path: "../../swift-primitives/swift-async-primitives")
     ],
     targets: [
         .target(
             name: "Effects",
             dependencies: [
                 .product(name: "Effect Primitives", package: "swift-effect-primitives"),
-                .product(name: "Dependency Primitives", package: "swift-dependency-primitives"),
+                .product(name: "Dependency Primitives", package: "swift-dependency-primitives")
             ]
         ),
         .target(
             name: "Effects Built-in",
             dependencies: [
-                "Effects",
+                "Effects"
             ]
         ),
         .target(
             name: "Effects Testing",
             dependencies: [
                 "Effects",
-                .product(name: "Async Primitives", package: "swift-async-primitives"),
+                .product(name: "Async Primitives", package: "swift-async-primitives")
             ]
-        ),
-        .testTarget(
-            name: "Effects Tests",
-            dependencies: [
-                "Effects",
-                "Effects Testing",
-            ]
-        ),
-        .testTarget(
-            name: "Effects Built-in Tests",
-            dependencies: [
-                "Effects Built-in",
-                "Effects Testing",
-                .product(name: "Async Primitives", package: "swift-async-primitives"),
-            ]
-        ),
-        .testTarget(
-            name: "Effects Testing Tests",
-            dependencies: [
-                "Effects Testing",
-            ]
-        ),
+        )
     ],
     swiftLanguageModes: [.v6]
 )
@@ -82,7 +61,7 @@ for target in package.targets where ![.system, .binary, .plugin, .macro].contain
         .enableUpcomingFeature("InternalImportsByDefault"),
         .enableUpcomingFeature("MemberImportVisibility"),
         .enableExperimentalFeature("Lifetimes"),
-        .strictMemorySafety(),
+        .strictMemorySafety()
     ]
     target.swiftSettings = (target.swiftSettings ?? []) + settings
 }
